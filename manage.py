1c61460a1e5314742785d6343acf6d74dd879822
NvRay <769007157@qq.com>
2016-08-08 17:20:02 +0800

    Ver 0.0

--- manage.py
+++ manage.py
@@ -1,18 +1,25 @@
-from flask import Flask
-from flask_script import Shell
-from flask_migrate import MigrateCommand,Migrate,Manager
-from app import create_app
-from app.models import db
+# from flask import Flask
+# from flask_script import Shell
+# from flask_migrate import MigrateCommand,Migrate,Manager
+# from app import create_app
+# from app.models import db
 
 
-# app=create_app()
-# app.debug=True
-app=Flask(__name__)
-@app.route("/")
-def index():
-    return "success!"
-migrate=Migrate(app,db)
-manager=Manager(app)
-manager.add_command("db",MigrateCommand)
-if __name__ =="__main__":
-    app.run()
+# # app=create_app()
+# # app.debug=True
+# app=Flask(__name__)
+# @app.route("/")
+# def index():
+#     return "success!"
+# migrate=Migrate(app,db)
+# manager=Manager(app)
+# manager.add_command("db",MigrateCommand)
+# if __name__ =="__main__":
+#     app.run()
+from flask import Flask  
+app = Flask(__name__)  
+@app.route('/')  
+def hello():  
+    return "Hello World!"  
+if __name__ ==  '__main__':  
+    app.run() 
