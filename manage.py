ff638173bc802a3cc4200fe691aa2062c5c50530
NvRay <769007157@qq.com>
2016-08-08 16:38:57 +0800

    Ver 1.4.2

--- manage.py
+++ manage.py
@@ -1,11 +1,16 @@
+from flask import Flask
 from flask_script import Shell
 from flask_migrate import MigrateCommand,Migrate,Manager
 from app import create_app
 from app.models import db
 
 
-app=create_app()
+# app=create_app()
 # app.debug=True
+app=Flask(__name__)
+@app.route("/")
+def index():
+    return "success!"
 migrate=Migrate(app,db)
 manager=Manager(app)
 manager.add_command("db",MigrateCommand)
