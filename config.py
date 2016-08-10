a942a2b5eea5afacd54d3167bfdc414735b8dc96
NvRay <769007157@qq.com>
2016-08-10 16:39:46 +0800

    t1.6

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 # coding:utf-8
 import os
+import sae.const
 
 
 class Configuration(object):
     SECRET_KEY = "c61f1f9e762e14h654dd"
     CHARSET = 'utf8'
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "mysql+pymysql://2d5cdc9f4f044fa29d9ef8e37d40a09b:982f80a0d86c4acf94a444ef9fb0a2de@sqld.duapp.com:4050/DzBhYPfwAgVQexRZklPL"#"mysql+pymysql://oz5z1y2nxz:l252hiklh3jj5ymwm35z0345454lk42wmkihhzh5@r.rdc.sae.sina.com.cn:3307/app_nvray"  #os.getenv("sql",default="mysql+pymysql://root:@127.0.0.1/myblog")  
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "mysql+pymysql://{u}:{p}@{h}:{P}/{db}".format(u=sae.const.MYSQL_USER,p=sae.const.MYSQL_PASS,h=sae.const.MYSQL_HOST,P=sae.const.MYSQL_PORT,db=sae.const.MYSQL_DB)  #os.getenv("sql",default="mysql+pymysql://root:@127.0.0.1/myblog")  
     SQLALCHEMY_MIGRATE_REPO = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'db_repository')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
