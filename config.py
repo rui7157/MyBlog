dcd8dcb0376bf1c0e0f344a5ac2e15f2d47345f5
NvRay <769007157@qq.com>
2016-08-10 15:39:09 +0800

    sea 0.1

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 class Configuration(object):
     SECRET_KEY = "c61f1f9e762e14h654dd"
     CHARSET = 'utf8'
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or  os.getenv("sql",default="mysql+pymysql://root:@127.0.0.1/myblog")  
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "mysql+pymysql://oz5z1y2nxz:l252hiklh3jj5ymwm35z0345454lk42wmkihhzh5@w.rdc.sae.sina.com.cn:3307/app_nvray"  #os.getenv("sql",default="mysql+pymysql://root:@127.0.0.1/myblog")  
     SQLALCHEMY_MIGRATE_REPO = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'db_repository')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
