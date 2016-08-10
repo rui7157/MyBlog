eebedda76b0a7f548e8a277bcbf27dc44a21b5a6
NvRay <769007157@qq.com>
2016-08-10 16:04:42 +0800

    t1.2

--- manage.py
+++ manage.py
@@ -6,16 +6,7 @@ from app import create_app
 from app.models import db
 
 
-# app=create_app()
-app=Flask(__name__)
-@app.route("/")
-def index():
-    return u"第一页"
-
-@app.route("/2")
-def index2():
-    return u"第二页"
-
+app=create_app()
 app.debug=True
 migrate=Migrate(app,db)
 manager=Manager(app)
